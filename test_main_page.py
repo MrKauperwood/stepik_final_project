968093eb91cc30ac68550b7f29ab9ea40d3fa22b
Alexey <lexabondrec@gmail.com>
2019-09-29 17:02:31 +0300

    Added exception handling (NoSuchElementException)

--- test_main_page.py
+++ test_main_page.py
@@ -4,4 +4,4 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
+    page.should_be_login_link()
